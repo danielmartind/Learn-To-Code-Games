68444e5ee05a13caf955655babc364b8f47d5e9b
Dan Martin <dan.martin@solidroute.co.uk>
2016-10-29 16:59:36 +0100

    Finish Number Wizard Create and start Text 101 Add UI text and image

--- Number Wizard/Assets/Scripts/NumberWizard.cs
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -2,59 +2,59 @@
 using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
-    int max;
-    int min;
-    int guess;
+	int max;
+	int min;
+	int guess;
 
-    // Use this for initialization
-    void Start () {
-        StartGame();
+	// Use this for initialization
+	void Start () {
+		StartGame();
 	}
 
-    void StartGame()
-    {
-        max = 1000;
-        min = 1;
-        guess = 500;
-        
+	void StartGame()
+	{
+		max = 1000;
+		min = 1;
+		guess = 500;
+		
 
-        print("========================");
-        print("Welcome to Number Wizard");
-        print("Pick a number in your head, but don't tell me!");
+		print("========================");
+		print("Welcome to Number Wizard");
+		print("Pick a number in your head, but don't tell me!");
 
-        print("The highest number you can pick is " + max);
-        print("The lowest number you can pick is " + min);
+		print("The highest number you can pick is " + max);
+		print("The lowest number you can pick is " + min);
 
-        print("Is the number higher or lower than " + guess + "?");
-        print("up = higher, down = lower or enter = correct");
-        max = max + 1;
-    }   
+		print("Is the number higher or lower than " + guess + "?");
+		print("up = higher, down = lower or enter = correct");
+		max = max + 1;
+	}   
 
-    // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            //print("Up arrow pressed");
-            min = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            //print("Down arrow pressed");
-            max = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            print("I Won!");
-            StartGame();
-        }
-    }
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			//print("Up arrow pressed");
+			min = guess;
+			NextGuess();
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			//print("Down arrow pressed");
+			max = guess;
+			NextGuess();
+		}
+		else if (Input.GetKeyDown(KeyCode.Return))
+		{
+			print("I Won!");
+			StartGame();
+		}
+	}
 
-    void NextGuess()
-    {
-        guess = (max + min) / 2;
-        print("Is the number higher or lower than " + guess + "?");
-        print("up = higher, down = lower or enter = correct");
-    }
+	void NextGuess()
+	{
+		guess = (max + min) / 2;
+		print("Is the number higher or lower than " + guess + "?");
+		print("up = higher, down = lower or enter = correct");
+	}
 }
