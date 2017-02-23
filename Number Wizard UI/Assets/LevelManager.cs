46637f63302c3e92a5fc8e8046a757c8c0f9f27d
Dan Martin <dan.martin@solidroute.co.uk>
2017-02-23 19:46:22 +0000

    Load Scenes and Quit

--- Number Wizard UI/Assets/LevelManager.cs
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -6,10 +6,12 @@ public class LevelManager : MonoBehaviour {
  	public void LoadLevel(string name)
     {
         Debug.Log("Level load requested for: " + name);
+        Application.LoadLevel(name);
     }
 
     public void QuitRequest()
     {
         Debug.Log("Quit Requested");
+        Application.Quit();
     }
 }
