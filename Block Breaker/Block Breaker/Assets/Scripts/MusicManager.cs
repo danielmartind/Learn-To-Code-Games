d5ae2ba78e9e027a82711fa3fb13ed5d18a976b6
Dan Martin <dan.martin@solidroute.co.uk>
2017-03-05 21:31:35 +0000

    fix music glitch add background add ball

--- Block Breaker/Block Breaker/Assets/Scripts/MusicManager.cs
+++ Block Breaker/Block Breaker/Assets/Scripts/MusicManager.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour {
 
-    public static MusicManager Instance = null;
-    // Use this for initialization
-    void Start () {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-            print("Destroyed!");
-        }
-        else
-        {
-            Instance = this;
-            GameObject.DontDestroyOnLoad(gameObject);
-        }
+	public static MusicManager Instance = null;
+
+	private void Awake()
+	{        
+		if (Instance != null)
+		{
+			Destroy(gameObject);
+			print("Destroyed!");
+		}
+		else
+		{
+			Instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+	   
 	}
 	
 	// Update is called once per frame
