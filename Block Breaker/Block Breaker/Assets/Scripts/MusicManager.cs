c17784d66ad31fcd3e60f73674719c92d44b1bc5
Dan Martin <dan.martin@solidroute.co.uk>
2017-03-05 14:31:33 +0000

    IMported menu added music

---
+++ Block Breaker/Block Breaker/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public static MusicManager Instance = null;
+    // Use this for initialization
+    void Start () {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            print("Destroyed!");
+        }
+        else
+        {
+            Instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
